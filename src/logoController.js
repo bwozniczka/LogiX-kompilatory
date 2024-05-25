d3fa03d97a68f31a1608cf075c43e0b1a4b09bd7
Bartłomiej Woźniczka <wozniczka.bart@gmail.com>
2024-05-25 15:03:04 +0200

    Adding new components to the layout

--- src/logoController.js
+++ src/logoController.js
@@ -1,26 +1,29 @@
-import LogiXLexer from './grammar/LogiXLexer.js';
-import LogiXParser from './grammar/LogiXParser.js';
-import LogiXVisitor from './grammar/LogiXVisitor.js';
-import antlr4 from 'antlr4';
-import testCodes from './assets/test_codes.json';
+import LogiXLexer from "./grammar/LogiXLexer.js";
+import LogiXParser from "./grammar/LogiXParser.js";
+import LogiXVisitor from "./grammar/LogiXVisitor.js";
+import antlr4 from "antlr4";
+import testCodes from "./assets/test_codes.json";
 
 export const initiateBeaver = (logoScreen, beaver) => {
   logoScreen.canvas.width = 400;
   logoScreen.canvas.height = 400;
   logoScreen.save();
-  logoScreen.translate(logoScreen.canvas.width / 2, logoScreen.canvas.height / 2);
-  logoScreen.rotate(270 * Math.PI / 180);
+  logoScreen.translate(
+    logoScreen.canvas.width / 2,
+    logoScreen.canvas.height / 2
+  );
+  logoScreen.rotate((270 * Math.PI) / 180);
   logoScreen.drawImage(beaver, -15, -15, 30, 30);
   logoScreen.restore();
-}
+};
 
 export const moveBeaver = (beaver, ctx, x, y, deg) => {
   ctx.save(); //Zapisanie stanu sprzed narysowania bobra i transformacji
   ctx.translate(x, y); //Przeniesienie bobra w dobre miejsce
-  ctx.rotate(deg * Math.PI / 180); //Rotacja o określony kąt
+  ctx.rotate((deg * Math.PI) / 180); //Rotacja o określony kąt
   ctx.drawImage(beaver, -15, -15, 30, 30); //Narysowanie bobra
   ctx.restore(); //Odzyskanie linii przejścia na obrazku
-}
+};
 
 export const parseLogo = (input, visitor) => {
   const chars = new antlr4.InputStream(input);
@@ -28,16 +31,16 @@ export const parseLogo = (input, visitor) => {
   const tokens = new antlr4.CommonTokenStream(lexer);
   const parser = new LogiXParser(tokens);
 
-  parser.buildParseTrees = true;  // Ensure parse trees are built
+  parser.buildParseTrees = true; // Ensure parse trees are built
 
-  const tree = parser.prog();  // Assuming 'prog' is the start rule in your grammar
+  const tree = parser.prog(); // Assuming 'prog' is the start rule in your grammar
   console.log(tree.toStringTree(parser.ruleNames, parser));
   visitor.visit(tree);
 };
 
 export const loadInstructions = (name, target) => {
   target.value = testCodes[name];
-}
+};
 
 export function saveCanvasAsImage(canvas) {
   const url = canvas.toDataURL("image/png");
